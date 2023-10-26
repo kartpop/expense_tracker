9747b4b314a617e63f0b85b5db7ae1f679ec4b5d
KartikeyaPophali <kartikeyapophali@gmail.com>
2023-10-26 11:57:12 +0530

    feat: add dummy expenses screen

---
+++ lib/expenses.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _Expenses();
+  }
+}
+
+class _Expenses extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [Text('The chart'), Text('Expenses list...')],
+      ),
+    );
+  }
+}
