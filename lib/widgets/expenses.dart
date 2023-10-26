2107782fb18b744a6c0a66359c438b311a8a2afa
KartikeyaPophali <kartikeyapophali@gmail.com>
2023-10-26 17:46:05 +0530

    feat: display NewExpense modal bottom sheet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -7,11 +8,11 @@ class Expenses extends StatefulWidget {
 
   @override
   State<Expenses> createState() {
-    return _Expenses();
+    return _ExpensesState();
   }
 }
 
-class _Expenses extends State<Expenses> {
+class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
     Expense(
         title: 'Flutter Course',
@@ -25,12 +26,24 @@ class _Expenses extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: Column(
         children: [
